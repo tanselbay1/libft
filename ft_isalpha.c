5520d84afcdaef2d17480dcbc8841485a8dfee19
Tansel Bayraktaroglu <tbayrakt@z2t14c3.42bangkok.com>
2024-02-16 14:42:10 +0700

    complete ft_isalpha

---
+++ ft_isalpha.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbayrakt <tbayrakt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 16:50:18 by tbayrakt          #+#    #+#             */
+/*   Updated: 2024/02/16 14:40:35 by tbayrakt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <ctype.h>
+// #include <stdio.h>
+
+int	ft_isalpha(char c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	return (0);
+}
+
+// int	main(void)
+// {
+// 	int	isAlpha;
+// 	int	isAlphaOrg;
+
+// 	isAlpha = ft_isalpha(97);
+// 	isAlphaOrg = isalpha(97);
+// 	printf("Is there any alfabetical char? %d\n", isAlpha);
+// 	printf("Is there any alfabetical char? %d\n", isAlphaOrg);
+// 	return (0);
+// }
