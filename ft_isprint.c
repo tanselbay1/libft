b55d0ab38997fbfba6ac9dca8c18641aa43dbd9f
Tansel Bayraktaroglu <tbayrakt@z2t14c3.42bangkok.com>
2024-02-17 16:18:09 +0700

    complete ft_isprint

---
+++ ft_isprint.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbayrakt <tbayrakt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 16:03:43 by tbayrakt          #+#    #+#             */
+/*   Updated: 2024/02/17 16:16:26 by tbayrakt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <ctype.h>
+// #include <stdio.h>
+
+int	ft_isprint(int c)
+{
+	if (c > 32 && c < 127)
+		return (1);
+	return (0);
+}
+
+// int	main(void)
+// {
+// 	int isPrint;
+// 	int isPrintOrg;
+
+// 	isPrint = ft_isprint(33);
+// 	isPrintOrg = isprint(33);
+// 	printf("Is there any printable character? %d\n", isPrint);
+// 	printf("Is there any printable character? %d\n", isPrintOrg);
+// 	return (0);
+// }
